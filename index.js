a89ebf3ddbc3021e112698ee4699ad665039b1ab
star01234 <111554691+star01234@users.noreply.github.com>
2023-01-29 20:39:42 +0700

    20.39

---
+++ index.js
@@ -0,0 +1,29 @@
+//ประกาศฟังก์ชั่น
+function appendImageElement(keyword, index){
+    const imgElement = document.createElement("img");
+    imgElement.className = "img"+index;
+    imgElement.src = `https://source.unsplash.com/225x225/?${keyword}&sig=${index}`;
+    const galleryElement = document.querySelector(".gallery");
+    galleryElement.appendChild(imgElement);
+}
+function removeAllImage(){
+    const galleryElement = document.querySelector(".gallery");
+    galleryElement.innerHTML = "";
+}
+function searchPhotos(event){
+    const keyword = event.target.value;
+    if(event.key === "Enter" && keyword){
+        removeAllImage();
+        for(let i = 1; i<=20; i++){
+            appendImageElement(keyword, i);
+        }
+    }
+}
+
+function main(){
+    const inputElement = document.getElementsByClassName("search");
+    inputElement[0].addEventListener("keydown", searchPhotos);
+}
+
+//เรียกใช้ฟังก์ชั่น
+main();
